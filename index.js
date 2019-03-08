000f7ad94b3ffef502f230f922e2675726be8837
Derek Houck <derek@derekhouck.com>
2019-03-08 08:03:47 -0800

    Add DELETE endpoints

--- index.js
+++ index.js
@@ -10,6 +10,50 @@ const { PORT, CLIENT_ORIGIN } = require('./config');
 
 const app = express();
 
+const cats = [
+  {
+    imageURL: 'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+    name: 'Fluffy',
+    sex: 'Female',
+    age: 2,
+    breed: 'Bengal',
+    story: 'Thrown on the street'
+  },
+  {
+    imageURL: 'https://lh3.googleusercontent.com/1T9fGpQrbg-doYqt5C-7amJMi3ojEpPc4T-1bRM0HswI_um_cnIc3TM7oTb_-N16SDckloFs70TMIIHQvoQRUfiLi8fjFZpjwP2LiMsGsW4g3GEHB_cV-KaKpaNJPRa2k7-ENfAmKBO8vnt5SIQu5K8AhGwlFt09N0YaF_I59KGIvOsiaxI1MsTyWbKiye_0XN_pK20YZFVaODnswZtbstEbezn9GMAdbg54nu0fabJKHRTiVDxe1s3wKjIWOFLCpOGA9eAQXKw5FnINjAP_Kz1_v6v-Ut2oVb578INGtZxVBWvGOfPwmtfRlXSSZVuFIX7TcJWFRX0tRhkfN2FZaeo5BRGWJ4Ik6wQDPjiwquysFy85v6SDy0CdmCmSBZt6ug5A7fBsxsrlKlDYf3Rd_CvuXCxlsEctDb-ZICfam5SgUMDBziB6m2KeSB3g1QCS_CTaoLcGSexZtWV9bloABszwRzFHuvdeGOC3P_7faL6r9YVXVp3HQUX7p1fYm_S1Y7wtLYwezdijlHSqUdwjy7-2Jm5Z8xsMGcuI-ogDP5g3Xqs8sqKBrLLxJV3V2LpUS27D_pIMiAM1Axu-7On23gPAHChJrwrFYE9T7cncWe4PrRyTG2jEfSuBq6eOAqyRq_LvM823lJEjsJqvlk7kiAgDsgKwF10ccUWMANIyS0UpJJD33Asz44qU19ydLnDWHjrcYW5w3dxJVMCYoB-65zuYDQ=w960-h720-no',
+    imageDescription: 'White cat lounging on a black sofa',
+    name: 'Snowflake',
+    sex: 'Female',
+    age: 8,
+    breed: 'White',
+    story: 'Lived with three generations of the Taylor/Houck family. Very chatty.'
+  },
+  {
+    imageURL: 'https://www.softpaws.com/template/images/landing_page/july_cat_image.jpg',
+    imageDescription: 'A cat wearing sunglasses',
+    name: 'Tomcat Cruise',
+    sex: 'Male',
+    age: 4,
+    breed: 'Tabby',
+    story: 'Is too cool for school. Loves jumping on couches.'
+  }
+];
+
+const dogs = [
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+    name: 'Zeus',
+    sex: 'Male',
+    age: 3,
+    breed: 'Golden Retriever',
+    story: 'Owner Passed away'
+  },
+  { imageURL: 'https://i.imgur.com/PwRafnN.jpg', imageDescription: 'A sleepy shiba laying in a ball', name: 'Mila', sex: 'Female', age: 3, breed: 'Shiba Inu', story: 'Too beautiful for owner' },
+  { imageURL: 'https://i.imgur.com/uKnMvyp.jpg', imageDescription: 'A holy husky walking on water', name: 'Hank', sex: 'Male', age: 3, breed: 'Husky', story: 'Scared owner after turning kibble to chicken' }
+];
+
 app.use(
   morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
     skip: (req, res) => process.env.NODE_ENV === 'test'
@@ -23,29 +67,21 @@ app.use(
 );
 
 app.get('/api/cat', (req, res, next) => {
-  const cat = {
-    imageURL: 'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
-    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
-    name: 'Fluffy',
-    sex: 'Female',
-    age: 2,
-    breed: 'Bengal',
-    story: 'Thrown on the street'
-  };
-  return res.json(cat);
+  return res.json(cats[0]);
+});
+
+app.delete('/api/cat', (req, res, next) => {
+  cats.shift();
+  return res.sendStatus(204);
 });
 
 app.get('/api/dog', (req, res, next) => {
-  const dog = {
-    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
-    imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
-    name: 'Zeus',
-    sex: 'Male',
-    age: 3,
-    breed: 'Golden Retriever',
-    story: 'Owner Passed away'
-  };
-  return res.json(dog);
+  return res.json(dogs[0]);
+});
+
+app.delete('/api/dog', (req, res, next) => {
+  dogs.shift();
+  return res.sendStatus(204);
 });
 
 function runServer(port = PORT) {
