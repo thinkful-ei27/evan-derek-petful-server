a6421ad0ac26147eee70416cc9e5101b9dfafa89
Derek Houck <derek@derekhouck.com>
2019-03-08 12:51:57 -0800

    Switch to queues

---
+++ queue.js
@@ -0,0 +1,52 @@
+'use strict';
+
+class _Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+  }
+
+  enqueue(data) {
+    const node = new _Node(data);
+
+    if (this.first === null) {
+      this.first = node;
+    }
+
+    if (this.last) {
+      node.next = this.last;
+      this.last.prev = node;
+    }
+
+    this.last = node;
+  }
+
+  dequeue() {
+    if (this.first === null) {
+      return;
+    }
+
+    const node = this.first;
+    this.first = node.prev;
+
+    if (node === this.last) {
+      this.last = null;
+    }
+
+    return node.value;
+  }
+
+  peek() {
+    return this.first !== null ? this.first.value : null;
+  }
+}
+
+module.exports = Queue;
